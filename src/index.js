75b3baf63fba9866aa5c84ea32b0f5d125eb12d9
Tony <tonyivanovak@gmail.com>
2023-08-25 22:25:46 +0300

    Added fonts and basic styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./fonts/helveticaneuecyr-heavy.woff2";
+import "./fonts/Stolzl-Medium.woff2";
+import "./fonts/Stolzl-Regular.woff2";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
